68280dc943cb2c3cbdd619ea6d5e9226497be9e0
moonRider <solar.pom@gmail.com>
2024-05-16 16:42:11 +0800

    feat: update UISchema and add object.layout

--- schemas/ui_schema/example/ui_schema.ts
+++ schemas/ui_schema/example/ui_schema.ts
@@ -1,12 +1,81 @@
-import { UISchema, FormOptions } from '../mod.ts'
+import { UISchema } from '../mod.ts'
 
-const PersonFormUISchema: UISchema = {
+const PersonFormUISchema: UISchema<
+  {
+    name: string
+    gender: string
+    birthday: string
+    profession: string
+    sports: Array<string>
+    signature: string
+  },
+  {
+    md: 1200
+  }
+> = {
   // props pass to form
   'ui:options': {
     submitButtonOptions: {
       norender: true,
     },
-  } as Partial<FormOptions>,
+    layout: {
+      gap: [3, 6],
+      columns: 12,
+      screens: {
+        md: 1200,
+      },
+      groups: [
+        {
+          title: 'group A',
+          rows: {
+            /**
+             * ---------------------------------------------------
+             * | name                   | gender                 |
+             * |-------------------------------------------------|
+             * | birthday               |                        |
+             * |------------------------| signature              |
+             * | professsion| sports    |                        |
+             * ---------------------------------------------------
+             */
+            default: [
+              [
+                {
+                  span: 6,
+                  field: 'name',
+                },
+                {
+                  span: 6,
+                  field: 'gender',
+                },
+              ],
+              [
+                {
+                  span: 6,
+                  field: 'birthday',
+                },
+                {
+                  span: 6,
+                  field: 'signature',
+                  rowSpan: 2,
+                },
+              ],
+              [
+                {
+                  span: 3,
+                  field: 'profession',
+                },
+                {
+                  span: 'fill',
+                  field: 'sports',
+                },
+              ],
+            ],
+            md: [],
+          },
+        },
+      ],
+    },
+  },
   name: {
     'ui:widget': 'SingleLineWidget',
     'ui:placeholder': '请输入',
--- schemas/ui_schema/mod.ts
+++ schemas/ui_schema/mod.ts
@@ -9,51 +9,92 @@ interface SubmitButtonOptions {
   }
 }
 
-type FieldUIOptions = Record<string, unknown>
-
-// 未完全定义所有支持的属性，这里主要罗列了我们可能会使用的属性
-interface FieldOptions {
-  // title 与 description 属于可选属性。
-  // 如果没有定义，则使用 JSONSchema 定义的 title/description
-  title: string
-  description: string
-  disabled: boolean
-  readonly: boolean
-  // 是否隐藏错误的显示
-  hideError: boolean
-  // 控制 label 的展示
-  label: boolean
-  // 定义字段的显示顺序，这个为可选项，可能会使用 layout 来替代这个能力
-  // order 属性只适用于 object 的类型
-  order: Array<string>
+interface LayoutCell<O extends object> {
+  // fill 表示占剩余的 1 份，如果同一行有多个定义为 fill，那么他们应该等分。
+  // 如果同一行有元素隐藏了，那么 fill 会扩张
+  // 为 nunber 时，表示占宽度占据的栅格数
+  span: number | 'fill'
+  field: keyof O
+  rowSpan?: number
 }
 
-export interface FormOptions extends FieldOptions {
+interface LayoutGroup<O extends object, S extends Record<string, number>> {
+  title: string
+  rows: Record<'default' | keyof S, Array<Array<LayoutCell<O>>>>
+}
+
+interface ObjectPropertiesLayout<
+  O extends object,
+  S extends Record<string, number>
+> {
+  // 定义栅格数量
+  columns: number
+  // m <==> [m, m]
+  gap: number | [number, number]
+  // 定义断点
+  screens: S
+  groups: Array<LayoutGroup<O, S>>
+}
+
+interface ObjectFieldUIOptions<
+  O extends object,
+  S extends Record<string, number>
+> {
+  layout?: ObjectPropertiesLayout<O, S>
+  // 定义字段的显示顺序，这个为可选项，与 layout 互斥
+  order?: Array<string>
+}
+
+type FieldUIOptions<O extends object, S extends Record<string, number>> =
+  | ObjectFieldUIOptions<O, S>
+  | Record<string, unknown>
+
+// 未完全定义所有支持的属性，这里主要罗列了我们可能会使用的属性
+interface BasicOptions {
+  // title 与 description 属于可选属性。
+  // 如果没有定义，则使用 JSONSchema 定义的 title/description
+  'ui:title': string
+  'ui:description': string
+  'ui:disabled': boolean
+  'ui:readonly': boolean
+  // 是否隐藏错误的显示
+  'ui:hideError': boolean
+  // 控制 label 的展示
+  'ui:label': boolean
+}
+
+interface FormOptions<O extends object, S extends Record<string, number>>
+  extends ObjectFieldUIOptions<O, S> {
   submitButtonOptions: Partial<SubmitButtonOptions>
 }
 
-interface FieldUISchema extends FieldOptions {
+interface FieldSchema<O extends object, S extends Record<string, number>>
+  extends BasicOptions {
   /**
    * ui:option.xxxx 与 ui:xxxx 是等价的
    * 以下写成 FieldUIOptions & Partial<FieldOptions> 的目的是区分开
    * 内置支持的 Options 与自定义的 ui options，通常我们期望将内置的 options
    * 写成 ui:xxxx。ui:options 中只保留自定义的 ui options
    */
-  'ui:options': FieldUIOptions & Partial<FieldOptions>
+  'ui:options': FieldUIOptions<O, S>
   'ui:widget': string // enum of FieldType
   'ui:placeholder': string
   // 未完全罗列所有的 ui 参数
   // 具体参考 https://rjsf-team.github.io/react-jsonschema-form/docs/api-reference/uiSchema
 }
 
+type FieldsSchema<O extends object, S extends Record<string, number>> = {
+  [K in keyof O]: Partial<FieldSchema<O, S>>
+}
+
 /**
  * UISchema 定义了 Form 与 Fields 等相关的 UI 设置
- *
- * 以下用 TS 表达不出来，ts 会认为 'ui:options' 与 index signature 不兼容
- * 这里想表达的意思就是 ui:options 是定义给 form 使用，其余的定义给 field 使用
  */
-export interface UISchema {
-  // 传递给 Form 的 options
-  'ui:options': Partial<FormOptions>
-  [fieldName: string]: Partial<FieldUISchema>
-}
+export type UISchema<
+  O extends object,
+  S extends Record<string, number>
+> = Partial<BasicOptions> &
+  FieldsSchema<O, S> & {
+    // 传递给 Form 的 options
+    'ui:options': Partial<FormOptions<O, S>>
+  }
