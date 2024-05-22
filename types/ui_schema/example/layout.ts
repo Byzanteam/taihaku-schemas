221e86e74f827f33b5bb54cb6c01a144aff3ed71
moonRider <solar.pom@gmail.com>
2024-05-22 12:57:04 +0800

    feat: define layout schema

---
+++ types/ui_schema/example/layout.ts
@@ -0,0 +1,163 @@
+import type { UISchema } from '../mod.ts'
+
+const LayoutedFormUISchema: UISchema<{
+  name: string
+  signature: string
+  birthday: string
+  gender: string
+  address: string
+  profile: {
+    nickname: string
+    email: string
+  }
+  tags: Array<{ title: string; color: string }>
+}> = {
+  /**
+   * Outer layout
+   * ----------------------------------------
+   * | name       | profile    | signature  |
+   * |-------------------------|            |
+   * | birthday   | gender     |            |
+   * |--------------------------------------|
+   * | address           | tags             |
+   * ----------------------------------------
+   */
+  'ui:x-layout': [
+    {
+      type: 'Horizontal',
+      children: [
+        {
+          type: 'Vertical',
+          children: [
+            {
+              type: 'Horizontal',
+              children: [
+                {
+                  type: 'ContentElement',
+                  props: {
+                    content: 'This is a content element',
+                  },
+                },
+                {
+                  type: 'Field',
+                  property: 'name',
+                },
+                {
+                  type: 'Field',
+                  property: 'profile',
+                },
+              ],
+            },
+            {
+              type: 'Div',
+              props: {
+                style: {
+                  display: 'inline-block',
+                },
+              },
+              children: [
+                {
+                  type: 'Field',
+                  property: 'birthday',
+                },
+                {
+                  type: 'Field',
+                  property: 'gender',
+                },
+              ],
+            },
+          ],
+        },
+        {
+          type: 'Field',
+          property: 'signature',
+        },
+      ],
+    },
+    {
+      type: 'Horizontal',
+      children: [
+        {
+          type: 'Field',
+          property: 'address',
+        },
+        {
+          type: 'Field',
+          property: 'tags',
+        },
+      ],
+    },
+  ],
+  name: {
+    'ui:widget': 'SingleLineWidget',
+    'ui:placeholder': '姓名',
+    'ui:label': false,
+  },
+  profile: {
+    // same to 'ui:x-layout': [Field<nickname>, Field<email>]
+    // but without className defined
+    'ui:order': ['nickname', 'email'],
+    nickname: {
+      'ui:widget': 'SingleLineWidget',
+      'ui:placeholder': '昵称',
+      'ui:label': false,
+    },
+  },
+  signature: {
+    'ui:widget': 'SignatureWidget',
+    'ui:x-penColor': 'black',
+    'ui:x-height': 200,
+  },
+  birthday: {
+    'ui:widget': 'DateWidget',
+    'ui:x-format': 'yyyy-MM-dd',
+  },
+  gender: {
+    'ui:widget': 'RadioButtonWidget',
+    'ui:x-display': 'select',
+    'ui:placeholder': '性别',
+    'ui:label': false,
+  },
+  address: {
+    'ui:widget': 'SingleLineWidget',
+    'ui:placeholder': '家庭住址',
+    'ui:label': false,
+  },
+  tags: {
+    // you can define ui:options to tags property of person
+    // like disable person.tags's label
+    // 'ui:label': false
+    items: {
+      /**
+       * Inner layout
+       * -----------------------
+       * | title    | color    |
+       * -----------------------
+       */
+      'ui:x-layout': {
+        type: 'Horizontal',
+        children: [
+          {
+            type: 'Field',
+            property: 'title',
+          },
+          {
+            type: 'Field',
+            property: 'color',
+            children: [],
+          },
+        ],
+      },
+      title: {
+        'ui:widget': 'SingleLineWidget',
+        'ui:placeholder': '请输入',
+      },
+      color: {
+        'ui:widget': 'RadioButtonWidget',
+        'ui:x-display': 'select',
+      },
+    },
+  },
+}
+
+export default LayoutedFormUISchema
--- types/ui_schema/mod.ts
+++ types/ui_schema/mod.ts
@@ -12,9 +12,46 @@ interface SubmitButtonOptions {
   }
 }
 
-type ObjectFieldUIOptions = {
-  'ui:order'?: Array<string>
+type ObjectData = Record<string, unknown>
+
+type LayoutFieldElementType = 'Field'
+// Exclude is not working as expected
+// https://github.com/microsoft/TypeScript/issues/47178
+type LayoutElementType = Exclude<string, LayoutFieldElementType>
+
+interface LayoutBaseElement<T extends string = string> {
+  className?: string
+  type: T
 }
+
+/** The container for fields */
+interface LayoutFieldElement<O extends ObjectData>
+  extends LayoutBaseElement<LayoutFieldElementType> {
+  property: keyof O
+}
+
+/** The custom element */
+interface LayoutCustomElement<O extends ObjectData>
+  extends LayoutBaseElement<LayoutElementType> {
+  props?: ObjectData
+  children?: Array<LayoutElement<O>>
+}
+
+type LayoutElement<O extends ObjectData> =
+  | LayoutFieldElement<O>
+  | LayoutCustomElement<O>
+
+type ObjectFieldUIOptions<O extends ObjectData> = {
+  'ui:order'?: Array<keyof O>
+  // x-layout 存在时，order 将无效
+  'ui:x-layout'?: LayoutElement<O> | Array<LayoutElement<O>>
+}
+// TODO: define array field options
+// deno-lint-ignore ban-types
+type ArrayFieldUIOptions = {
+  // 'ui:x-hideAddButton'?: boolean
+}
+
 /**
  * @link https://rjsf-team.github.io/react-jsonschema-form/docs/api-reference/uiSchema
  */
@@ -29,8 +66,8 @@ type BasicUIOptions = {
   'ui:enumDisabled'?: Array<string | number>
 }
 
-type FormUIOptions =
-  & ObjectFieldUIOptions
+type FormUIOptions<O extends ObjectData> =
+  & ObjectFieldUIOptions<O>
   & BasicUIOptions
   & {
     'ui:submitButtonOptions'?: Partial<SubmitButtonOptions>
@@ -45,26 +82,33 @@ type FieldUIOptions<T extends FieldType> = T extends FieldType
   } & BasicUIOptions
   : never
 
-type FieldsUISchema<
-  O extends Record<string, unknown>,
-  T extends FieldType = FieldType,
-> = {
+type FieldsUISchema<O extends ObjectData, T extends FieldType = FieldType> = {
   [K in keyof O]: O[K] extends object // 嵌套结构（object or array)
     ? O[K] extends Array<infer U> // array
-      ? U extends Record<string, unknown>
-        ? { items: Partial<FieldsUISchema<U>> } // Array<object>
-      : FieldUIOptions<T> // normal array field like checkbox
-    : O[K] extends Record<string, unknown> ? Partial<FieldsUISchema<O[K]>> // nested object
+      ? U extends ObjectData ?
+          & {
+            /** define ArrayItem's uiSchema */
+            items: Partial<FieldsUISchema<U>> & ObjectFieldUIOptions<U>
+          }
+          & ArrayFieldUIOptions
+          & BasicUIOptions // Array<object>
+      : FieldUIOptions<T> & ArrayFieldUIOptions // normal array field like checkbox
+    : O[K] extends ObjectData
+      ? Partial<FieldsUISchema<O[K]>> & ObjectFieldUIOptions<O[K]> // nested object
     : FieldUIOptions<T> // fallback type
     : FieldUIOptions<T> // normal field
 }
 
-export type UISchema<O extends Record<string, unknown>> =
-  & Partial<
-    FieldsUISchema<O>
-  >
-  & Partial<FormUIOptions>
+export type UISchema<O extends ObjectData> =
+  & Partial<FieldsUISchema<O>>
+  & Partial<FormUIOptions<O>>
 
-export { type CustomFieldUIOptionsMap, FieldType }
+export type {
+  ArrayFieldUIOptions,
+  CustomFieldUIOptionsMap,
+  ObjectFieldUIOptions,
+}
+
+export { FieldType }
 
 export { AppearanceValue, type OptionColumns } from './ui_options.ts'
