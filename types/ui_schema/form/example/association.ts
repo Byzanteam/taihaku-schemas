a8784fc7ba48a06a1e8abd5c45ca57488686b9be
moonRider <solar.pom@gmail.com>
2024-08-05 18:31:11 +0800

    feat: and association field and CustomUIOptionMap example

---
+++ types/ui_schema/form/example/association.ts
@@ -0,0 +1,46 @@
+import type { UISchema } from '../mod.ts'
+
+const MovieFormUISchema: UISchema<
+  {
+    name: string
+    director?: {
+      id: string // format: 'uuid'
+      name?: string
+    }
+    poster?: {
+      id?: string // format: 'uuid'
+      name: string
+    }
+    actors: Array<{ id: string; name: string }>
+  },
+  /** Custom Widget UIOption Map */
+  {
+    FileWidget: {
+      'ui:x-multiple': boolean
+      'ui:x-accept': string
+    }
+  }
+> = {
+  'ui:rootFieldId': 'CreateMovieForm',
+  name: {
+    'ui:widget': 'SingleLineWidget',
+    'ui:placeholder': '请输入',
+  },
+  director: {
+    'ui:widget': 'BelongsToWidget',
+    'ui:x-association-resource': 'director',
+    'ui:x-display-property': 'name',
+  },
+  poster: {
+    'ui:widget': 'FileWidget',
+    'ui:x-multiple': false,
+    'ui:x-accept': 'image/*',
+  },
+  actors: {
+    'ui:widget': 'HasManyWidget',
+    'ui:x-association-resource': 'star',
+    'ui:x-display-property': 'name',
+  },
+}
+
+export default MovieFormUISchema
