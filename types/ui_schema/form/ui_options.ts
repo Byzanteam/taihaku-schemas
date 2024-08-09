cc8ccb6cc9056045d0f9f088a876368ae3d913c2
moonRider <solar.pom@gmail.com>
2024-08-09 13:19:28 +0800

    fixup! feat: add uiOptions to AssociationField's Input mode

--- types/ui_schema/form/ui_options.ts
+++ types/ui_schema/form/ui_options.ts
@@ -58,16 +58,20 @@ type AssociationFieldUIOptions = {
   /** which property should be shown as current assoication row value */
   'ui:x-display-property'?: string
   // 以下选项仅 Input 模式生效
-  'ui:x-search-placeholder'?: string
-  'ui:x-no-result-hint'?: string
-  /** default: 50 */
-  'ui:x-page-size'?: number
+  'ui:x-candidate-search-placeholder'?: string
+  /** 备选项搜索无果的提示 */
+  'ui:x-candidate-no-result-hint'?: string
+  /**
+   * 备选项分页尺寸
+   * @default 50
+   */
+  'ui:x-candidate-page-size'?: number
   /**
    * content 的 className，用来指定 popover content 宽度与 trigger 宽度相同
    * 但是因为该 class 的实现只能在应用端，所以需要通过参数传递
-   * default: 'popover-trigger-width'
+   * @default 'popover-trigger-width'
    */
-  'ui:x-popover-content-class-name'?: string
+  'ui:x-candidate-popover-content-class-name'?: string
 } & CommonCustomFieldUIOptions
 
 export type CustomFieldUIOptionsMap = {
