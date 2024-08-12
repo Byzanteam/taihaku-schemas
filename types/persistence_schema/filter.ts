5b5fec08d55f00e729e2f996617e605c0a611924
shaddollxz <shaddollxz@163.com>
2024-08-12 10:27:21 +0800

    refactor: filter-operator be enum

--- types/persistence_schema/filter.ts
+++ types/persistence_schema/filter.ts
@@ -1,26 +1,43 @@
-import type { DataPointer, SchemaPointer, ValuePointer } from './types.ts'
-export type Filter = ConditionalFilter | LogicalFilter
+import type { DataPointer, SchemaPointer, ValuePointer } from "./types.ts";
+export type Filter = ConditionalFilter | LogicalFilter;
 
-export interface ConditionalFilter {
-  operator: 'and' | 'or'
-  operands: Array<ConditionalFilter | LogicalFilter>
+export enum ConditionalFilterOperator {
+  AND = "and",
+  OR = "or",
 }
 
-export type LogicalFilter = EQFilter | LTFilter | IsNullFilter
+export enum LogicalFilterOperator {
+  CO = "co",
+  EQ = "eq",
+  LT = "lt",
+  IS_NULL = "is_null",
+}
+
+export interface ConditionalFilter {
+  operator: ConditionalFilterOperator;
+  operands: Array<ConditionalFilter | LogicalFilter>;
+}
+
+export type LogicalFilter = COFilter | EQFilter | LTFilter | IsNullFilter;
+
+export interface COFilter {
+  operator: LogicalFilterOperator.CO;
+  operands: [Operand, Operand];
+}
 
 export interface EQFilter {
-  operator: 'eq'
-  operands: [Operand, Operand]
+  operator: LogicalFilterOperator.EQ;
+  operands: [Operand, Operand];
 }
 
 export interface LTFilter {
-  operator: 'lt'
-  operands: [Operand, Operand]
+  operator: LogicalFilterOperator.LT;
+  operands: [Operand, Operand];
 }
 
 export interface IsNullFilter {
-  operator: 'is_null'
-  operands: [Operand]
+  operator: LogicalFilterOperator.IS_NULL;
+  operands: [Operand];
 }
 
-export type Operand = DataPointer | SchemaPointer | ValuePointer
+export type Operand = DataPointer | SchemaPointer | ValuePointer;
