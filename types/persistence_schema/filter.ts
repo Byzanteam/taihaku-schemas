55f3fb27f45254d67140250e0286c405c538dad9
solar.pom <solar.pom@gmail.com>
2025-05-09 14:49:52 +0800

    feat: add other filter operator (#62)

--- types/persistence_schema/filter.ts
+++ types/persistence_schema/filter.ts
@@ -10,7 +10,13 @@ export enum LogicalFilterOperator {
   CO = 'co',
   EQ = 'eq',
   LT = 'lt',
+  LTE = 'lte',
+  GT = 'gt',
+  GTE = 'gte',
   IS_NULL = 'is_null',
+  OV = 'ov',
+  SW = 'sw',
+  EW = 'ew',
 }
 
 export interface ConditionalFilter {
@@ -18,7 +24,17 @@ export interface ConditionalFilter {
   operands: Array<ConditionalFilter | LogicalFilter>
 }
 
-export type LogicalFilter = COFilter | EQFilter | LTFilter | IsNullFilter
+export type LogicalFilter =
+  | COFilter
+  | EQFilter
+  | LTFilter
+  | LTEFilter
+  | GTFilter
+  | GTEFilter
+  | IsNullFilter
+  | OVFilter
+  | SWFilter
+  | EWFilter
 
 export interface COFilter {
   operator: `${LogicalFilterOperator.CO}`
@@ -35,9 +51,39 @@ export interface LTFilter {
   operands: [Operand, Operand]
 }
 
+export interface LTEFilter {
+  operator: `${LogicalFilterOperator.LTE}`
+  operands: [Operand, Operand]
+}
+
+export interface GTFilter {
+  operator: `${LogicalFilterOperator.GT}`
+  operands: [Operand, Operand]
+}
+
+export interface GTEFilter {
+  operator: `${LogicalFilterOperator.GTE}`
+  operands: [Operand, Operand]
+}
+
 export interface IsNullFilter {
   operator: `${LogicalFilterOperator.IS_NULL}`
   operands: [Operand]
+}
+
+export interface OVFilter {
+  operator: `${LogicalFilterOperator.OV}`
+  operands: [Operand, Operand]
+}
+
+export interface SWFilter {
+  operator: `${LogicalFilterOperator.SW}`
+  operands: [Operand, Operand]
+}
+
+export interface EWFilter {
+  operator: `${LogicalFilterOperator.EW}`
+  operands: [Operand, Operand]
 }
 
 export type Operand = DataPointer | SchemaPointer | ValuePointer
